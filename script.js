af4ee39c45de19d0c21919458479b1e12f65cc10
SamuLucas <samulucasf@hotmail.com>
2022-01-17 23:17:53 -0300

    Entendendo laço de repetição na prática

---
+++ script.js
@@ -0,0 +1,31 @@
+//exercises 1
+
+let conta = 10;
+while (conta >= 1) {
+    console.log(conta)
+    conta--
+}
+
+//exercises 2
+
+let conta = 1;
+while (conta <= 100) {
+    console.log(conta)
+    conta += 2
+}
+
+for (let conta = 1; conta <= 100; conta += 2) {
+    console.log(conta)
+}
+
+//exercises 3
+
+let conta = 0;
+while (conta <= 100) {
+    console.log(conta)
+    conta += 2
+}
+
+for (let conta = 0; conta <= 100; conta += 2) {
+    console.log(conta)
+}
